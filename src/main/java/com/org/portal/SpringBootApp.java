d27c8cd4901afc3d9def78aa5a5d29b77a963809
anthonilawrance <anthonilawrance@gmail.com>
2019-11-13 20:56:59 +0530

    Pom.xml

--- src/main/java/com/org/portal/SpringBootApp.java
+++ src/main/java/com/org/portal/SpringBootApp.java
@@ -2,11 +2,16 @@ package com.org.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootApp {
+public class SpringBootApp extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp.class, args);
 		System.out.println("I am calling...");
+	}
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootApp.class);
 	}
 }
